35e5761b1fda68becce6e6d7326de598c5161692
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-05-10 22:08:11 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int sum=0;
-        int sub=0;
+        int odd=0;
+        int even=0;
         for(int i=0; i<num_list.length; i++){
             if(i%2==0){
-                sum+=num_list[i];
+                odd+=num_list[i];
             } else {
-                sub+=num_list[i];
+                even+=num_list[i];
             }
         }
-        return sum>sub ? sum : sub;
+        return odd>even ? odd : even;
     }
 }
