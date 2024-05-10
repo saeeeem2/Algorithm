6684ab0a794f47afd0dd8bf2a1c96e2ff3210edc
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-05-10 21:28:44 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = names.length%5==0 ? new String[names.length/5] : new String[names.length/5+1];
+        int idx=0;
+        for(int i=0; i<names.length; i=i+5){
+            answer[idx]=names[i];
+            idx++;
+        }
+        return answer;
+    }
+}
