388db236d1a349ec3700fd590c4389eb1eb4d117
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-06-04 18:09:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer=0;
+        while(num!=1){
+            if(num%2==0){
+                num=num/2;
+                answer++;
+            } else {
+                num=num*3+1;
+                answer++;
+            }
+            if(answer==500){
+                answer=-1;
+                break;
+            } 
+            if(num<=0){
+                answer=-1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
