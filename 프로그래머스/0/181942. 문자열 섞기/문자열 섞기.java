95930aa8b2091abeb13d528a76fb992053fd5636
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-06-21 18:30:25 +0900

    [level 0] Title: 문자열 섞기, Time: 1.88 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        String[] str1_list=str1.split("");
+        String[] str2_list=str2.split("");
+        for(int i=0; i<str1_list.length; i++){
+            answer+=str1_list[i];
+            answer+=str2_list[i];
+        }
+        return answer;
+    }
+}
