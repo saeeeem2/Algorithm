e5420fa32a0faab3e6f7b4f995b2c017e14cd8dc
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-08-13 23:37:48 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt=0;
+        int idx=1;
+        int num=n;
+        while(num!=1){
+            if(num%2==0){
+                num=num/2;
+                cnt++;
+            } else if(num%2!=0){
+                num=3*num+1;
+                cnt++;
+            } else {
+                break;
+            }
+        }
+        int[] answer = new int[cnt+1];
+        answer[0]=n;
+        while(n!=1){
+            if(n%2==0){
+                n=n/2;
+                answer[idx]=n;
+                idx++;
+            } else if(n%2!=0){
+                n=3*n+1;
+                answer[idx]=n;
+                idx++;
+            } else if(n==1){
+                answer[idx]=n;
+            }    
+        }
+        return answer;
+    }
+}
