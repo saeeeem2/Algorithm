2f608a01fe579987b7378bba8102110552e5c8ed
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-09-11 20:41:31 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.08 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int x=0;
+        for(int i=1; i<common.length-1; i++){
+            if(common[i]-common[i-1]==common[i+1]-common[i]){
+                x=common[i]-common[i-1];
+                answer=common[common.length-1]+x;
+            }else{
+                x=common[i]/common[i-1];
+                answer=common[common.length-1]*x;
+            }
+        }
+        return answer;
+    }
+}
