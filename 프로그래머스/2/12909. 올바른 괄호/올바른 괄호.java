98145f1f56b58cf0ea136ef6ae39f73cca3144cb
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-10-17 23:16:28 +0900

    [level 2] Title: 올바른 괄호, Time: 6.66 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                cnt++;
+            } else {
+                cnt--;
+            }
+            if(cnt<0){
+                answer=false;
+            }
+        }
+        if(cnt==0){
+            if(s.charAt(0)=='(' && s.charAt(s.length()-1)==')'){
+                return answer;
+            } else {
+                answer=false;
+            }
+        }else {
+            answer=false;
+        }
+            
+
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
