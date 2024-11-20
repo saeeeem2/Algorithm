11202e5b8b15cb239f792b55e9d2f39ea15df5c9
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-11-20 21:49:20 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        if(num1==num2){
-            answer = 1;
-        } else {
-            answer = -1;
-        }
+        int answer = (num1==num2) ? 1:-1;
         return answer;
     }
 }
