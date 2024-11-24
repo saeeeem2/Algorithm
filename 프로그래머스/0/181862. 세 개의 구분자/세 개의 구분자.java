091065051b2d58329c7014608beaa31eb6884769
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-11-24 20:41:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 69.81 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,11 +1,26 @@
 class Solution {
     public String[] solution(String myStr) {
-        String[] answer;
-        String s = myStr.replaceAll("[a-c]", " ");
-        if (s.trim().isEmpty()) {
-            answer = new String[]{"EMPTY"};
-        } else {
-            answer = s.trim().split("\\s+");
+        myStr=myStr.replaceAll("[abc]"," ");
+        String[] myString = myStr.split(" ");
+        int idx=0;
+        int cnt=1;
+        for(int i=0; i<myString.length; i++){
+            if(!myString[i].equals("")){
+                cnt++;
+            }
+        }
+        String[] answer=new String[cnt-1]; 
+        if(cnt==1){
+            answer=new String[1]; 
+            answer[0]="EMPTY";
+        }
+        for(int i=0; i<myString.length; i++){
+            if(!myString[i].equals("")){
+                answer[idx]=myString[i];
+                idx++;
+            } else {
+                answer[idx]="EMPTY";
+            }
         }
         return answer;
     }
