fc84a88ce2f6b95d5123ae1aca9036ccb6167684
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2024-11-29 06:05:07 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/README.md
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 피자 나눠 먹기 (2) - 120815 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120815) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120815#qna) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 0.03 ms
+메모리: 75.9 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 22일 22:46:55
+2024년 11월 29일 06:05:04
 
 ### 문제 설명
 
--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,19 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int min=0;
-        int gcd=0;
-        if(6<=n){
-            min=6;
-        } else {
-            min=n;
-        }
-        for(int i=1; i<=min; i++){
-            if(6%i==0 && n%i==0){
-                gcd=i;
+        
+        for (int i = 1; i <= 6 * n; i++) {
+            if (6 * i % n == 0) {
+                answer = i;
+                break;
             }
         }
-        answer=(6*n)/gcd/6;
+        
         return answer;
     }
 }
