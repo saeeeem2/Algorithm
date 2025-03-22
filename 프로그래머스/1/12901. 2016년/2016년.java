4c0a3f6b33840f52addf26eae26d6fb09a93e113
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-03-22 19:58:32 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int day = 0;
+        for(int i=1; i<a; i++){
+            if(i==2){
+                day += 29;
+            }else if(i==4 || i==6 || i==9 || i==11){
+                day += 30;
+            }else{
+                day += 31;
+            }
+        }
+        day += b - 1;
+        String str[] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        answer = str[day%7];
+        return answer;
+    }
+}
