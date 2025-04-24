97d56a24e284bf125b4e252ac072a79854065b3c
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-04-24 10:40:06 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.62 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int list[]={a,b,c,d};
+        Arrays.sort(list);
+        if(list[0]==list[3]){
+            answer=1111*list[0];
+        } else if(list[0]==list[1]&&list[1]==list[2]&&list[2]!=list[3]){
+            answer=(int)Math.pow((10*list[0]+list[3]),2);
+        } else if(list[3]==list[1]&&list[1]==list[2]&&list[2]!=list[0]){
+            answer=(int)Math.pow((10*list[3]+list[0]),2);
+        } else if(list[0]==list[1] && list[2]==list[3]){
+            answer=(list[0]+list[2])*Math.abs(list[0]-list[2]);
+        } else if(list[0]==list[1]&&list[2]!=list[3]){
+            answer=list[2]*list[3];
+        } else if(list[1]==list[2]&&list[0]!=list[3]){
+            answer=list[0]*list[3];
+        } else if(list[2]==list[3]&&list[0]!=list[1]) {
+            answer=list[0]*list[1]; 
+        } else if(list[0]!=list[3]&&list[1]!=list[2]&&list[0]!=list[1]){
+            answer=list[0];
+        }    
+        return answer;
+    }
+}
