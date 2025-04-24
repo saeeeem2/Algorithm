34535077434b76ade99bbb543995aa50df7b281b
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-04-24 10:43:22 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.57 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -4,21 +4,30 @@ class Solution {
         int answer = 0;
         int list[]={a,b,c,d};
         Arrays.sort(list);
+        //다 같을 때
         if(list[0]==list[3]){
             answer=1111*list[0];
-        } else if(list[0]==list[1]&&list[1]==list[2]&&list[2]!=list[3]){
+        } 
+        //숫자 3개가 같을 때
+        else if(list[0]==list[1]&&list[1]==list[2]&&list[2]!=list[3]){
             answer=(int)Math.pow((10*list[0]+list[3]),2);
         } else if(list[3]==list[1]&&list[1]==list[2]&&list[2]!=list[0]){
             answer=(int)Math.pow((10*list[3]+list[0]),2);
-        } else if(list[0]==list[1] && list[2]==list[3]){
+        }
+        //숫자 2개씩 같을 때
+        else if(list[0]==list[1] && list[2]==list[3]){
             answer=(list[0]+list[2])*Math.abs(list[0]-list[2]);
-        } else if(list[0]==list[1]&&list[2]!=list[3]){
+        } 
+        //두 숫자가 같고 나머지 두 숫자가 서로 다를 때
+        else if(list[0]==list[1]&&list[2]!=list[3]){
             answer=list[2]*list[3];
         } else if(list[1]==list[2]&&list[0]!=list[3]){
             answer=list[0]*list[3];
         } else if(list[2]==list[3]&&list[0]!=list[1]) {
             answer=list[0]*list[1]; 
-        } else if(list[0]!=list[3]&&list[1]!=list[2]&&list[0]!=list[1]){
+        } 
+        //다 다를 때
+        else {
             answer=list[0];
         }    
         return answer;
