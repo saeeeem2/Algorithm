4bb2e19853048aaad01a0ed0078f5821ecbf789e
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-05-03 21:19:01 +0900

    [level 0] Title: qr code, Time: 3.68 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(int q, int r, String code) {
         String answer = "";
-        String[] code_idx=code.split("");
-        for(int i=0; i<code_idx.length; i++){
-            if(i%q==r){
-                answer+=code_idx[i];
-            }
+        String str[] = code.split("");
+        for(int i=r; i<str.length; i=i+q){
+            answer += str[i];
         }
         return answer;
     }
