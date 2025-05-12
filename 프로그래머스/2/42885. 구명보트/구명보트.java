c11d20cee228ba3ad74082dc0f00ff2ab849d700
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-05-12 22:24:03 +0900

    [level 2] Title: 구명보트, Time: 11.66 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int idx=people.length-1;
+        int i=0;
+        while(i<=idx){
+            if(people[i]+people[idx]<=limit){
+                i++;
+            }
+            idx--;
+            answer++;
+        }
+        return answer;
+    }
+}
