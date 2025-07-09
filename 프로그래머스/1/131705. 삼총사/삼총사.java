9ab385651acb3f654d4d6e1560cbba6c928492a4
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-07-09 14:46:05 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0; i<number.length; i++){
             for(int j=i+1; j<number.length; j++){
                 for(int k=j+1; k<number.length; k++){
-                    if(i!=j&&i!=k&&j!=k&&number[i]+number[j]+number[k]==0){
+                    if(number[i]+number[j]+number[k] == 0){
                         answer++;
                     }
                 }
