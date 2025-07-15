957424fe78c591b61080476cfc8b4ea9fd30dbac
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-07-15 22:37:23 +0900

    [level 2] Title: 카펫, Time: 33.75 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = new int[2];
         for(int i=1; i<=brown; i++){
             for(int j=1; j<=brown; j++){
-                if((2*i-4)+(2*j)==brown && i<=j && i*j-brown==yellow){
+                if((2*i-4)+(2*j) == brown && i <= j && i*j-brown == yellow){
                     answer[0]=j;
                     answer[1]=i;
                     break;
