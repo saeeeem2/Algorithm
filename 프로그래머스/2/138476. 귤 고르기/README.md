19f756fed1568af7e04913ed63884f06b3e581f7
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-07-19 01:08:59 +0900

    [level 2] Title: 귤 고르기, Time: 56.29 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 90.4 MB, 시간: 88.75 ms
+메모리: 88.3 MB, 시간: 56.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 23일 11:40:27
+2025년 07월 19일 01:08:56
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,21 +3,17 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         Map<Integer, Integer> countMap = new HashMap<>();
-
         for (int size : tangerine) {
             countMap.put(size, countMap.getOrDefault(size, 0) + 1);
         }
-
         List<Integer> counts = new ArrayList<>(countMap.values());
         counts.sort(Collections.reverseOrder());
-
         int sum = 0;
         for (int c : counts) {
             sum += c;
             answer++;
             if (sum >= k) break;
         }
-        
         return answer;
     }
 }
