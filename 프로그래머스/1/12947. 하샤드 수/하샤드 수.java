a9042b4745a0289d8ff766f811e322b785e35113
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-07-25 11:13:16 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,8 +3,8 @@ class Solution {
         boolean answer = true;
         int num=0;
         for(int i=x; i>0; i=i/10){
-            num+=i%10;
+            num += i%10;
         }
-        return x%num==0;
+        return x % num == 0;
     }
 }
