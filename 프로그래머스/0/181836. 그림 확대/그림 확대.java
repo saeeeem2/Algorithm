ca6e67e33d15b8bc324974d016b13eb6776e4b3c
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-07-30 00:28:33 +0900

    [level 0] Title: 그림 확대, Time: 17.53 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -6,12 +6,12 @@ class Solution {
             answer[idx]="";
             for(int j=0; j<picture[i].length(); j++){
                 for(int l=0; l<k; l++){
-                    answer[idx]+=picture[i].charAt(j);
+                    answer[idx] += picture[i].charAt(j);
                 }
             }
             idx++;
             for(int m=0; m<k-1; m++){
-                answer[idx]=answer[idx-1];
+                answer[idx] = answer[idx-1];
                 idx++;
             }
         }
