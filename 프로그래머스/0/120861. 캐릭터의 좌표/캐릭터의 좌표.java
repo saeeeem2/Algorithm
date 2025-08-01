a2280d6ae337b98abc14bb733683ea28c1a0f919
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-08-02 00:05:38 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = new int[2];
-        int x=board[0]/2;
-        int y=board[1]/2;
+        int x = board[0]/2;
+        int y = board[1]/2;
         for(int i=0; i<keyinput.length; i++){
             if(keyinput[i].equals("right") && answer[0] != x){
                 answer[0]++;
