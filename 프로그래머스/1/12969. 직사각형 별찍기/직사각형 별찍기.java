d7a5d48249f87ef8b0a955a803d954b7288bcbf0
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-08-03 12:25:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 283.62 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -5,11 +5,11 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        for(int i=0; i<b; i++){
-            for(int j=0; j<a; j++){
-                
+        for(int i = 0; i < b; i++){
+            for(int j = 0; j < a; j++){
                 System.out.print("*");
             }
+            
             System.out.println();
         }
     }
