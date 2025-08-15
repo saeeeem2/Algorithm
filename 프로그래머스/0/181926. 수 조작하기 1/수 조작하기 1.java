22a0b248d029c495d388a619fcdb77542d08ba76
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-08-15 13:07:53 +0900

    [level 0] Title: 수 조작하기 1, Time: 10.99 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int n, String control) {
-        int answer = n;
+        int answer=n;
         for(int i=0; i<control.length(); i++){
-            if(control.charAt(i)=='w'){
-                answer+=1;
-            } else if(control.charAt(i)=='s'){
-                answer-=1;
-            } else if(control.charAt(i)=='d'){
-                answer+=10;
+            if(control.charAt(i) == 'w'){
+                answer += 1;
+            } else if(control.charAt(i) == 's'){
+                answer -= 1;
+            } else if(control.charAt(i) == 'd'){
+                answer += 10;
             } else {
-                answer-=10;
+                answer -= 10;
             }
         }
         return answer;
