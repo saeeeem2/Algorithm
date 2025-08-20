96a0c6883ea6a5876e954ae4e5084351ef313f1d
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-08-21 00:47:26 +0900

    [level 0] Title: 369게임, Time: 7.05 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int order) {
-        String str = order+"";
-        String str2 = str.replaceAll("[0124578]","");
+        String str = order + "";
+        String str2 = str.replaceAll( "[0124578]" , "" );
         int answer = str2.length();
         return answer;
     }
