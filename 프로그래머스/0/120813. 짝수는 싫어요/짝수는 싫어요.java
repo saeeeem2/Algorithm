91015ce663c09d923a5cb3dabb3add985f170132
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-08-25 11:48:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,8 +3,8 @@ class Solution {
         int[] answer=(n%2==0) ? new int[n/2] : new int[n/2+1];
         int idx=0;
         for(int i=1; i<=n; i++){
-            if(i%2!=0){
-                answer[idx]=i;
+            if(i%2 != 0){
+                answer[idx] = i;
                 idx++;
             }
         }
