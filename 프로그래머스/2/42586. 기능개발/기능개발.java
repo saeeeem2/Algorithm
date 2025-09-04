d18c9e3eb50802a6ee4e199432b324fb7d46e306
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-04 11:33:43 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,15 +4,12 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int n = progresses.length;
         int[] pgrList = new int[n];
-
         for (int i = 0; i < n; i++) {
             pgrList[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]); 
         }
-
         List<Integer> result = new ArrayList<>();
         int max = pgrList[0];
         int count = 1;
-
         for (int i = 1; i < n; i++) {
             if (pgrList[i] <= max) {
                 count++;
@@ -23,12 +20,10 @@ class Solution {
             }
         }
         result.add(count); 
-
         int[] answer = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             answer[i] = result.get(i);
         }
-
         return answer;
     }
 }
