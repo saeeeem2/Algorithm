166c19cf1c005d7d7d4227f5ad0f2a3942d82e8d
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-07 23:45:00 +0900

    [level 1] Title: 소수 찾기, Time: 25.81 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         boolean[] isPrime = new boolean[n + 1];
         for(int i=2; i<isPrime.length; i++){
-            isPrime[i]=true;
+            isPrime[i] = true;
         }
         for(int i=2; i*i<=n; i++){
             if(isPrime[i]){
