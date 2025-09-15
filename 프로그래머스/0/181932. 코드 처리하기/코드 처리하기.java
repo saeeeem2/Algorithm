37f83c112d865da7a81bbccee713e7fe5c80f543
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-15 13:45:39 +0900

    [level 0] Title: 코드 처리하기, Time: 385.85 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -3,23 +3,23 @@ class Solution {
         String answer = "";
         int mod=0;
         for(int i=0; i<code.length(); i++){
-            if(mod==0){
-                if(code.charAt(i)!='1' && i%2==0){
-                    answer+=code.charAt(i)+"";
+            if(mod == 0){
+                if(code.charAt(i) != '1' && i % 2 == 0){
+                    answer += code.charAt(i) + "";
                 } else if(code.charAt(i) == '1'){
-                    mod=1;
+                    mod = 1;
                 }
             } else if(mod == 1) {
-                if(code.charAt(i) != '1' && i%2 != 0){
-                    answer+=code.charAt(i)+"";
+                if(code.charAt(i) != '1' && i % 2 != 0){
+                    answer += code.charAt(i) + "";
                 } else if(code.charAt(i) == '1'){
-                    mod=0;
+                    mod = 0;
                 }
             }
         }
         
         if(answer.length() == 0){
-            answer="EMPTY";
+            answer = "EMPTY";
         }
         return answer;
     }
