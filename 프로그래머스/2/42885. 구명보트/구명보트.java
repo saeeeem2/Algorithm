e5dbf011c6063a59fc1fb5f511ec4a50f147254f
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-18 13:33:35 +0900

    [level 2] Title: 구명보트, Time: 11.09 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,10 +3,10 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int idx=people.length-1;
-        int i=0;
+        int idx = people.length-1;
+        int i = 0;
         while(i <= idx){
-            if(people[i]+people[idx]<=limit){
+            if(people[i]+people[idx] <= limit){
                 i++;
             }
             idx--;
