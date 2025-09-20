1634a7824267f0164be82a9c1b0812a459fe6c56
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-20 16:07:34 +0900

    [level 1] Title: 추억 점수, Time: 21.13 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer=new int[photo.length];
+        int[] answer = new int[photo.length];
         for(int i=0; i<photo.length; i++){
             for(int j=0; j<photo[i].length; j++){
                 for(int k=0; k<name.length; k++){
