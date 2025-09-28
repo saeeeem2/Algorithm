e4e78d623d091b22af8d721e7e201797ef21f29a
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-09-28 22:13:24 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 60.79 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,13 +2,13 @@ class Solution {
     public int solution(String number) {
         int answer = 0;
         String[] num=number.split("");
-        int n=0;
+        int n = 0;
         
         for(int i=0; i<num.length; i++){
             n+=Integer.parseInt(num[i]);
         }
         
-        answer=n%9;
+        answer = n%9;
         
         return answer;
     }
