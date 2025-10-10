af7a65ae2c0d5dd3a2a2ef5254082e24debdca25
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2025-10-10 16:32:44 +0900

    [level 0] Title: 암호 해독, Time: 18.88 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        
         for(int i=1; i<=cipher.length(); i++){
-            if(i%code==0){
-                answer+=cipher.charAt(i-1);
+            if(i % code ==0){
+                answer += cipher.charAt(i-1);
             }
         }
         
