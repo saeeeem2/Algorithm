3878ba508e5eea0267bf3a46a4433522c67770e1
saeeeem2 <32377963+saeeeem2@users.noreply.github.com>
2026-05-27 01:06:04 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요 
-SELECT
-        b.BOOK_ID as BOOK_ID , 
+SELECT 
+        b.BOOK_ID as BOOK_ID ,  
         a.AUTHOR_NAME as AUTHOR_NAME, 
         DATE_FORMAT(b.PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
 FROM BOOK b 
